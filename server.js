7d3c4eb4b403ede92844d2cc0fa4084141857a7d
MahmoudAhmed294 <60276598+MahmoudAhmed294@users.noreply.github.com>
2022-09-04 21:45:51 +0200

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const jsonServer = require('json-server')
+const dotenv = require('dotenv')
+
 const server = jsonServer.create()
 const router = jsonServer.router('company.json')
 const middlewares = jsonServer.defaults()
+dotenv.config()
 
 
 server.use(middlewares)
 server.use(router)
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
